98487179fdee63ccc915e67b982c75ccc1c07730
RuDra KannoJiya <45325778+rudrak07@users.noreply.github.com>
2023-12-05 18:48:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ async def predict(
         'confidence': confidence
     }
 
-# if __name__ == "__main__":
-    # uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
